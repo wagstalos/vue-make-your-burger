771d5d164900f9a1a48014d4e510caaf72d1a0c8
Wagner Paulo <wagstalos@hotmail.com>
2022-08-16 09:10:49 -0300

    feat: views router

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>footer</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="" alt="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+</style>
