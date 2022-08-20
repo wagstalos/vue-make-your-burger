2d3a8aaaec42e0a249fb17f404195241b2431e8d
Wagner Paulo <wagstalos@hotmail.com>
2022-08-20 17:07:13 -0300

    feat: navbar and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <p>footer</p>
-    </div>
+    <footer id="footer">
+        <p>Make your burguer &copy; 2022</p>
+    </footer>
 </template>
 
 <script>
@@ -9,3 +9,17 @@
         name: "Footer"
     }
 </script>
+
+<style scoped>
+#footer{
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    bottom: 0;
+}
+</style>
